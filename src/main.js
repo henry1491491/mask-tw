763b5feaa667152d67c6dffe5f2be800a46e6e1a
henry1491491 <40331438+henry1491491@users.noreply.github.com>
2020-04-21 16:23:18 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -19,28 +19,6 @@ Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 })
 
-/**
- * 功能：
- * 做出篩選縣市及行政區 => 已完成
- * ajax call 資料，先顯示藥局地圖資訊 => 已完成
- * 圖標圖層 cluster 套件 => 已完成
- * LeogJson
- *
- *
- * 問題紀錄：
- * Icon 物件源碼
- * Icon.Default 是什麼東西 => 應該是預設的 icon 圖標
- * 什麼情況可以一次引入所有組件，就像 Vuetufy 那樣可以一次使用所有組件
- * this.$nextTick 跟 this.$refs 的引用
- * 初始化會出現沒有 markerList 而間接引發的沒有 markerCenter 問題
- * 圖層簡單一點，不要用到這麼複雜
- * marker icon custom
- *
- *
- * 筆記記錄：
- * 可以透過 myMap 這個物件的 mapObject 屬性去調用方法跟屬性
- */
-
 Vue.config.productionTip = false
 
 new Vue({
